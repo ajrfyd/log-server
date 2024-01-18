38efe0d9d24610c85475a5f5f520e3291759d545
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2024-01-18 14:02:17 +0900

    Update: temp

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 import express from "express";
+import "./config/init.js";
 import cors from "cors";
-import path from "path";
-import dotenv from "dotenv";
 import c from "chalk";
 import { log, __dirname } from "./utils/index.js";
 
-dotenv.config({
-  path: path.join(__dirname, "./.env")
-});
+// dotenv.config({
+//   path: path.join(__dirname, "./.env")
+// });
 
 const { PORT } = process.env;
 const app = express();
 
+app.use(cors({
+  methods: ["GET"],
+  origin: ["*"]
+}));
+
+app.get("/", (req, res) => {
+  log(c.bgCyan(__dirname));
+  res.send("<h1>Welcome!!</h1>");
+});
 
 app.listen(PORT, () => {
   log(c.bgBlue(`Server Listening on PORT: ${PORT}`));
